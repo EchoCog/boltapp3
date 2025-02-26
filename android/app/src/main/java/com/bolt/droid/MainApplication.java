5027ccc6435ac5ce2fca635d4c99c453438c74d8
mducko <marduk@rzo.io>
2025-02-26 10:13:04 +0000

    Assistant checkpoint: Optimized Android build performance and configuration
    Assistant generated file changes:
    - android/gradle.properties: Optimize Gradle properties for better Android performance
    - android/app/src/main/java/com/bolt/droid/MainApplication.java: Optimize MainApplication for better performance
    - github/workflows/android.yaml: Optimize Android CI workflow
    
    ---
    
    User prompt:
    
    Optimize for android

--- android/app/src/main/java/com/bolt/droid/MainApplication.java
+++ android/app/src/main/java/com/bolt/droid/MainApplication.java
@@ -8,6 +8,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
